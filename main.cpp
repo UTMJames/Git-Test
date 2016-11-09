2cc735194340a18036aa02356de8929955136412
l33tJams <jamesmit@ut.utm.edu>
2016-11-08 18:03:21 -0600

    new main for the git_test project.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
-int main(){
+int sum(int number);
 
+int main(){
+  int n;
   cout << "Hello World" << endl;
+  cout << "Please enter a number: ";
+  cin >> n;
+  cout << "Sum: ";
+  cout << sum(n) << endl;
   
+  
+}
+
+int sum(int number){
+  int sum = 0;
+  for(int i = 1; i <= number; i++){
+    sum = sum + i;
+  }
+  return sum;
 }
